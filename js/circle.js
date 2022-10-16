96f4ad3d17b9c2a94ab58a09c8ffe890563b23bc
KekOnTheWorld <kekontheworld@gmail.com>
2022-10-16 17:31:40 +0200

    Made code prettier

--- js/circle.js
+++ js/circle.js
@@ -1,12 +1,13 @@
 import { init2D } from "./base.js";
 
 const radius = 10;
-const width = radius * 2,
-      height = radius * 2;
-const voxel2D = init2D(document.getElementById("canvas"), width + 1, height + 1);
+const width = radius * 2 + 1,
+      height = radius * 2 + 1;
+
+const voxel2D = init2D(document.getElementById("canvas"), width, height);
 
 voxel2D.map((_, x, y) => {
-    const _x = x - width / 2, _y = y - height / 2;
+    const _x = x - radius, _y = y - radius;
     return Math.round(Math.sqrt(_x*_x + _y*_y)) !== radius;
 });
 voxel2D.draw();
